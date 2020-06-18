79629a0b3683c76451f8fcf97a7e3121a8644340
Jeferson Leon <jefersonleonblue@gmail.com>
2020-06-18 14:38:04 -0300

    Aula LIVE dia 17/06 - tarde - Desafio 6 - Questão 3

---
+++ src/livetardedes6ques3/LiveTardeDes6Ques3.java
@@ -0,0 +1,35 @@
+package livetardedes6ques3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jeferson Leon
+ */
+public class LiveTardeDes6Ques3 {
+
+    /**
+     * Escrever um algoritmo que lê 10 valores, um de cada vez, e conta 
+     * quantos deles estão no intervalo [10,20] e quantos deles estão 
+     * fora do intervalo, escrevendo estas informações.
+     */
+    public static void main(String[] args) {
+        int cont, num, contDentro, contFora;
+        contDentro = 0;
+        contFora = 0;
+        for (cont = 1; cont <= 10; cont++) {
+            num = Integer.parseInt(
+                    JOptionPane.showInputDialog("Digite um nº"));
+            if(num>=10 && num<=20){
+                contDentro++;
+            }else{
+                contFora++;
+            }//fim do if
+        }//fim do loop
+        JOptionPane.showMessageDialog(null,"No intervalo de  [10,20] "
+                + "existem: "+contDentro+" e fora deste intervalo "
+                + "existem: "+contFora,"Resultados",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
